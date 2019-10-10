bde488ed0d2606720182d4676ec75e09f055ac49
hyeakki <hye.akshay@gmail.com>
2019-10-10 06:24:25 +0000

    formatted launch.sh

--- launch.sh
+++ launch.sh
@@ -1,13 +1,34 @@
 #!/bin/bash
 
-# Removing if any containers with given names are running
-docker rm -f $(docker ps | grep my_nginx | awk {'print $1'})
-docker rm -f $(docker ps | grep my_php | awk {'print $1'})
+loc=$(dirname "$0")
+cd $PWD/$loc
 
-# Removing previous images
-docker rmi -f $(docker images | grep my_nginx | awk {'print $3'})
-docker rmi -f $(docker images | grep my_php | awk {'print $3'})
+rm_old_conts() {
+	# Removing if any containers with given names are running
+	docker rm -f $(docker ps | grep my_nginx | awk {'print $1'})
+	docker rm -f $(docker ps | grep my_php | awk {'print $1'})
+}
 
-# Running and linking NGINX and PHP-FPM containers
-docker run -d --name my_php hiakki/my_php
-docker run -d --name my_nginx --link my_php:my_php -p 80:80 hiakki/my_nginx -v ~/logs:/var/log/nginx
+rm_old_imgs() {
+	# Removing previous images
+	docker rmi -f $(docker images | grep my_nginx | awk {'print $3'})
+	docker rmi -f $(docker images | grep my_php | awk {'print $3'})
+}
+
+new_conts() {
+	echo "Running and linking NGINX and PHP-FPM containers"
+	# Running and linking NGINX and PHP-FPM containers
+	docker run -d --name my_php hiakki/my_php
+	docker run -d --name my_nginx \
+        	      --link my_php:my_php \
+	               -p 80:80 \
+        	      --mount type=bind,source="$PWD"/nginx/logs,target=/var/log/nginx \
+			hiakki/my_nginx
+}
+
+echo "Removing if any containers with given names are running"
+rm_old_conts > /dev/null 2>&1
+echo "Removing previous images"
+rm_old_imgs > /dev/null 2>&1
+
+new_conts
