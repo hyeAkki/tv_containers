bd0b8fe32a59c3f8522bcdfc4717de2403f8b4ac
hyeakki <hye.akshay@gmail.com>
2019-10-11 18:27:00 +0000

    version 1.0.0

--- nginx/start.sh
+++ nginx/start.sh
@@ -6,9 +6,9 @@ sleep 5
 
 status=$?
 if [ $status -ne 0 ]; then
-  echo "Nginx Failed: $status"
+  echo "Nginx Failed: $status" > status1.txt
   exit $status
-  else echo "Starting Nginx: OK" > status1.txt
+  else echo "Starting Nginx: OK" > status2.txt
 fi
 
 sleep 2
@@ -17,16 +17,16 @@ while /bin/true; do
 
   ps aux | grep 'nginx: master process' | grep -q -v grep
   NGINX_STATUS=$?
-  echo "Checking NGINX, Status Code: $NGINX_STATUS" > status2.txt
+  echo "Checking NGINX, Status Code: $NGINX_STATUS" > status3.txt
   sleep 2
-   elif [ $NGINX_STATUS -ne 0 ];
+   if [ $NGINX_STATUS -ne 0 ];
      then
-       echo "$(date +%F_%T) FATAL: NGINX Raised a Status Code of $NGINX_STATUS and exited" > status3.txt
+       echo "$(date +%F_%T) FATAL: NGINX Raised a Status Code of $NGINX_STATUS and exited" > status4.txt
        exit -1
 
    else
      sleep 2
-        echo "$(date +%F_%T) - HealtCheck: NGINX: OK" > status4.txt
+        echo "$(date +%F_%T) - HealtCheck: NGINX: OK" > status5.txt
   fi
   sleep 2
 done
--- php/start.sh
+++ php/start.sh
@@ -6,25 +6,25 @@ sleep 5
 
 status=$?
 if [ $status -ne 0 ]; then
-  echo "php-fpm7 Failed: $status"
+  echo "php-fpm7 Failed: $status" > status1.txt
   exit $status
-  else echo "Starting PHP-FPM: OK" > status1.txt
+  else echo "Starting PHP-FPM: OK" > status2.txt
 fi
 
 while /bin/true; do
   ps aux | grep 'php-fpm: master process' | grep -q -v grep
   PHP_FPM_STATUS=$?
-  echo "Checking PHP-FPM, Status Code: $PHP_FPM_STATUS" > status2.txt
+  echo "Checking PHP-FPM, Status Code: $PHP_FPM_STATUS" > status3.txt
   sleep 2
 
   if [ $PHP_FPM_STATUS -ne 0 ];
     then
-      echo "$(date +%F_%T) FATAL: PHP-FPM Raised a Status Code of $PHP_FPM_STATUS and exited" > status3.txt
+      echo "$(date +%F_%T) FATAL: PHP-FPM Raised a Status Code of $PHP_FPM_STATUS and exited" > status4.txt
       exit -1
 
    else
      sleep 2
-        echo "$(date +%F_%T) - HealtCheck: PHP-FPM: OK" > status4.txt
+        echo "$(date +%F_%T) - HealtCheck: PHP-FPM: OK" > status5.txt
   fi
   sleep 2
 done
