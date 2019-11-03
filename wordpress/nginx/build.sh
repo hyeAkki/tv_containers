9c5de8b715d29df174977db2fbdebcbabd67a4b6
hyeakki <hye.akshay@gmail.com>
2019-11-03 18:56:10 +0000

    version 1.0.0

--- wordpress/nginx/build.sh
+++ wordpress/nginx/build.sh
@@ -8,10 +8,7 @@ IMAGE=my-nginx
 loc=$(dirname "$0")
 cd $PWD/$loc
 
-wget https://wordpress.org/latest.tar.gz
-tar xzf latest.tar.gz
-cp ../wp-config.php wordpress/wp-config.php
-cp ../test.php wordpress/test.php
+
+https://github.com/hyeAkki/tricksvibe.git
 docker build -t $USERNAME/$IMAGE .
-rm -rf wordpress
-rm -rf latest.tar.gz*
+rm -rf tricksvibe
