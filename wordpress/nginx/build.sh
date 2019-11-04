773b765f6b0aba52b8a14f9365fab16c0b039e81
hyeakki <hye.akshay@gmail.com>
2019-11-04 08:59:25 +0000

    version 1.0.0

--- wordpress/nginx/build.sh
+++ wordpress/nginx/build.sh
@@ -8,8 +8,19 @@ IMAGE=my-nginx
 loc=$(dirname "$0")
 cd $PWD/$loc
 
-
+case "$1" in
+tv)
 git clone https://github.com/hyeAkki/tricksvibe.git app
+;;
+hd)
+git clone https://github.com/hyeAkki/hdhxmovies.git app
+;;
+*)
+echo "Usage: sh build.sh <hd/tv>"
+exit
+;;
+esac
+
 cp ../test.php app/
 docker build -t $USERNAME/$IMAGE .
 rm -rf app
