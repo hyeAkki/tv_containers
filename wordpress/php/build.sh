4facb7f9124f58b3c025e54f2245166ec0a11a36
hyeakki <hye.akshay@gmail.com>
2019-11-08 09:09:54 +0000

    version 1.0.0

--- wordpress/php/build.sh
+++ wordpress/php/build.sh
@@ -8,19 +8,4 @@ IMAGE="$1-php"
 loc=$(dirname "$0")
 cd $PWD/$loc
 
-case "$1" in
-tv)
-git clone https://github.com/hyeAkki/tricksvibe.git app
-;;
-hd)
-git clone https://github.com/hyeAkki/hdhxmovies.git app
-;;
-*)
-echo "Usage: sh build.sh <hd/tv>"
-exit
-;;
-esac
-
-cp ../test.php app/
 docker build -t $USERNAME/$IMAGE .
-rm -rf app
--- wordpress/release.sh
+++ wordpress/release.sh
@@ -16,25 +16,43 @@ loc=$(dirname "$0")
 cd $PWD/$loc/nginx
 
 version=$(cat VERSION)
-echo "version: $version"
+
+
+case "$1" in
+tv)
+git clone https://github.com/hyeAkki/tricksvibe.git app
+sed -i 's/$host/www.tricksvibe.tk/g' ./conf/default.conf
+cp ../test.php app/
 sudo sh build.sh "$1"
+sed -i 's/www.tricksvibe.tk/$host/g' ./conf/default.conf
+;;
+hd)
+git clone https://github.com/hyeAkki/hdhxmovies.git app
+sed -i 's/$host/www.hdhxmovies.ga/g' ./conf/default.conf
+cp ../test.php app/
+sudo sh build.sh "$1"
+sed -i 's/www.hdhxmovies.ga/$host/g' ./conf/default.conf
+;;
+*)
+echo "Usage: sh build.sh <hd/tv>"
+exit
+;;
+esac
+
+echo "version: $version"
+
 # tag it
-git add -A
-git commit -m "version $version"
-git tag -a "$version" -m "version $version"
-git push
-git push --tags
 docker tag $USERNAME/$IMAGE:latest $USERNAME/$IMAGE:$version
 # push it
 docker push $USERNAME/$IMAGE:latest
 docker push $USERNAME/$IMAGE:$version
 
+mv app/ ../php/
 
 # image name
 IMAGE="$1-php"
 
-cd ..
-cd $PWD/$loc/php
+cd ../php
 
 version=$(cat VERSION)
 echo "version: $version"
@@ -45,14 +63,17 @@ then
   exit
 fi
 sudo sh build.sh "$1"
+
 # tag it
 git add -A
 git commit -m "version $version"
 git tag -a "$version" -m "version $version"
 git push
 git push --tags
+
 docker tag $USERNAME/$IMAGE:latest $USERNAME/$IMAGE:$version
 # push it
 docker push $USERNAME/$IMAGE:latest
 docker push $USERNAME/$IMAGE:$version
 
+rm -rf app
