809ad04413a6790133e955a40d633e81ef270b43
hyeakki <hye.akshay@gmail.com>
2019-11-08 09:15:23 +0000

    version 1.0.0

--- wordpress/release.sh
+++ wordpress/release.sh
@@ -64,16 +64,16 @@ then
 fi
 sudo sh build.sh "$1"
 
-# tag it
-git add -A
-git commit -m "version $version"
-git tag -a "$version" -m "version $version"
-git push
-git push --tags
-
 docker tag $USERNAME/$IMAGE:latest $USERNAME/$IMAGE:$version
 # push it
 docker push $USERNAME/$IMAGE:latest
 docker push $USERNAME/$IMAGE:$version
 
 rm -rf app
+
+# tag it
+git add -A
+git commit -m "version $version"
+git tag -a "$version" -m "version $version"
+git push
+git push --tags
